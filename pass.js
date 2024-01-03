de3cb9ace57b4ae0afe8b5d39cedff42dcbb9b0c
komal kumar <152193731+komalkumarpalwai@users.noreply.github.com>
2024-01-03 18:49:48 +0530

    Add files via upload

---
+++ pass.js
@@ -0,0 +1,10 @@
+function generatePassword(length = 12) {
+    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+[]{}|;:,.<>?';
+    let password = '';
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      password += characters.charAt(randomIndex);
+    }
+
+    document.getElementById('password-input').value = password;
+  }
